6b09b24b71978f975c09b144c1d8676e8e53318a
Dipikaaher6 <101876956+Dipikaaher6@users.noreply.github.com>
2023-06-14 14:32:45 +0530

    Update streamlite_app.py
    added 3 breakfast menu

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('Paratha')
-streamlit.text('Puri Bhaji')
-streamlit.text('Tea')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
